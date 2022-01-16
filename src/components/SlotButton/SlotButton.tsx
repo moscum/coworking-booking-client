e3e2bfa44fc6a49d754d4991f2e4db909304fe20
KingGamer322 <aaa.1.vvv.1@bk.ru>
2022-01-16 11:19:23 +0500

    fixed tooltip

--- src/components/SlotButton/SlotButton.tsx
+++ src/components/SlotButton/SlotButton.tsx
@@ -4,13 +4,13 @@ import { Button, Tooltip } from 'clcm';
 import cn from 'clsx';
 
 import { useDispatch, useSelector } from '@src/hooks';
-import { Reservation } from '@src/types';
+import { Reservation, User } from '@src/types';
 import {
   selectReservationState,
   setReservationDate,
   updateTimeSlots,
 } from '@store/reservation';
-import { selectTable, selectOtherDayTables } from '@store/table';
+import { selectTable } from '@store/table';
 
 interface Props {
   date: string;
@@ -20,74 +20,25 @@ interface Props {
 
 const SlotButton: React.VFC<Props> = ({ hour, date, reservations }) => {
   const [status, setStatus] = useState('disabled');
+  const [currentSlot, setCurrentSlot] = useState<User | null>(null);
   const reservationState = useSelector(selectReservationState);
-  const time = new Date(date);
-  const timeString = time.toLocaleDateString('sv');
-  time.setHours(hour);
-
   const selectedTable = useSelector(selectTable);
-  const tableId = selectedTable?.id;
-  const otherDayTables = useSelector(selectOtherDayTables);
-  const NextDay = otherDayTables[1]?.date;
-  const NextDay2 = otherDayTables[2]?.date;
-  const NextDay3 = otherDayTables[3]?.date;
+  const time = new Date(date);
+  time.setHours(hour);
+  // let index;
 
-  let ind: number;
-
-  if (hour < 10 && date === NextDay) {
-    ind = otherDayTables[1]?.tables[tableId!]?.findIndex(
-      (x) => x.date === `${NextDay}T0${hour}:00:00Z`
-    ) as number;
-  } else if (hour < 10 && date === NextDay2) {
-    ind = otherDayTables[2]?.tables[tableId!]?.findIndex(
-      (x) => x.date === `${NextDay2}T0${hour}:00:00Z`
-    ) as number;
-  } else if (hour < 10 && date === NextDay3) {
-    ind = otherDayTables[3]?.tables[tableId!]?.findIndex(
-      (x) => x.date === `${NextDay3}T0${hour}:00:00Z`
-    ) as number;
-  } else if (hour < 10 && date === timeString) {
-    ind = selectedTable?.reservations.findIndex(
-      (x) => x.date === `${date}T0${hour}:00:00Z`
-    ) as number;
-  } else if (date === NextDay) {
-    ind = otherDayTables[1]?.tables[tableId!]?.findIndex(
-      (x) => x.date === `${NextDay}T${hour}:00:00Z`
-    ) as number;
-  } else if (date === NextDay2) {
-    ind = otherDayTables[2]?.tables[tableId!]?.findIndex(
-      (x) => x.date === `${NextDay2}T${hour}:00:00Z`
-    ) as number;
-  } else if (date === NextDay3) {
-    ind = otherDayTables[3]?.tables[tableId!]?.findIndex(
-      (x) => x.date === `${NextDay3}T${hour}:00:00Z`
-    ) as number;
-  } else if (date === timeString) {
-    ind = selectedTable?.reservations.findIndex(
-      (x) => x.date === `${date}T${hour}:00:00Z`
-    ) as number;
-  }
-
-  let abc;
-  if (reservations) {
-    if (date === NextDay) {
-      abc = `${reservations[ind!]?.user.firstName} ${
-        reservations[ind!]?.user.lastName
-      }`;
-    } else if (date === NextDay2) {
-      abc = `${reservations[ind!]?.user.firstName} ${
-        reservations[ind!]?.user.lastName
-      }`;
-    } else if (date === NextDay3) {
-      abc = `${reservations[ind!]?.user.firstName} ${
-        reservations[ind!]?.user.lastName
-      }`;
-    } else if (date === timeString) {
-      abc = `${selectedTable?.reservations[ind!]?.user.firstName} ${
-        selectedTable?.reservations[ind!]?.user.lastName
-      }`;
-    }
-  }
+  // if (reservations) {
+  //   index = reservations.find((r) => {
+  //     return new Date(r.date!).getUTCHours() === hour;
+  //   });
+  //   console.log(
+  //     index?.user.firstName,
+  //     index?.user.lastName,
+  //     index?.date,
+  //     index?.hour,
+  //     index?.tableId
+  //   );
+  // }
 
   const dispatch = useDispatch();
 
@@ -100,6 +51,12 @@ const SlotButton: React.VFC<Props> = ({ hour, date, reservations }) => {
   };
 
   useEffect(() => {
+    let abc: User | null;
+    if (reservations) {
+      abc = reservations.find((r) => {
+        return new Date(r.date!).getUTCHours() === hour;
+      })?.user!;
+    }
     if (new Date().getTime() > time.getTime() || !selectedTable)
       setStatus('disabled');
     else if (
@@ -115,6 +72,7 @@ const SlotButton: React.VFC<Props> = ({ hour, date, reservations }) => {
       })
     ) {
       setStatus('busy');
+      setCurrentSlot(abc!);
     } else setStatus('free');
   }, [
     selectedTable,
@@ -124,33 +82,35 @@ const SlotButton: React.VFC<Props> = ({ hour, date, reservations }) => {
   ]);
 
   return (
-    <Tooltip
-      className="bg-gray-1 text-black whitespace-nowrap"
-      position="top"
-      disabled={status !== 'busy'}
-      content={abc}
-    >
-      <Button
-        className={cn(
-          'font-manrope text-lg w-14 p-0 rounded text-xl transition-all',
-          {
-            'bg-primary text-white': status === 'selected',
-            'bg-gray-1 hover:bg-blue-3 text-black': status === 'free',
-            'bg-gray-1 text-gray-2 cursor-default': status === 'disabled',
-            'bg-accent text-white cursor-not-allowed': status === 'busy',
-          }
-        )}
-        disabled={status !== 'free' && status !== 'selected'}
-        whileTap={
-          status !== 'free' && status !== 'selected'
-            ? undefined
-            : { scale: 0.95 }
-        }
-        onClick={handleClick}
+    <div className="group">
+      <Tooltip
+        className="invisible group-hover:visible bg-gray-1 text-black whitespace-nowrap"
+        position="top"
+        disabled={!currentSlot}
+        content={`${currentSlot?.firstName} ${currentSlot?.lastName}`}
       >
-        {`${hour < 10 ? '0' : ''}${hour}:00`}
-      </Button>
-    </Tooltip>
+        <Button
+          className={cn(
+            'font-manrope text-lg w-14 p-0 rounded text-xl transition-all',
+            {
+              'bg-primary text-white': status === 'selected',
+              'bg-gray-1 hover:bg-blue-3 text-black': status === 'free',
+              'bg-gray-1 text-gray-2 cursor-default': status === 'disabled',
+              'bg-accent text-white cursor-not-allowed': status === 'busy',
+            }
+          )}
+          disabled={status !== 'free' && status !== 'selected'}
+          whileTap={
+            status !== 'free' && status !== 'selected'
+              ? undefined
+              : { scale: 0.95 }
+          }
+          onClick={handleClick}
+        >
+          {`${hour < 10 ? '0' : ''}${hour}:00`}
+        </Button>
+      </Tooltip>
+    </div>
   );
 };
 
